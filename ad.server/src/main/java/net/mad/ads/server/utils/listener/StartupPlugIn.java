940a3f8052ff0c4c3b84ab9cec01a6de6cb0d024
thmarx <thmarx@gmx.net>
2011-09-04 15:40:24 +0200

    GPLv3 Header for every sourcefile, config location changed

--- ad.server/src/main/java/net/mad/ads/server/utils/listener/StartupPlugIn.java
+++ ad.server/src/main/java/net/mad/ads/server/utils/listener/StartupPlugIn.java
@@ -1,3 +1,20 @@
+/*
+ * Mad-Advertisement Copyright (C) 2011 Thorsten Marx
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.mad.ads.server.utils.listener;
 
 
@@ -62,10 +79,20 @@ public class StartupPlugIn implements ServletContextListener {
 			// Konfiguration einlesen
 			String enviroment = event.getServletContext().getInitParameter("enviroment");
 			
+			String configDirectory = event.getServletContext().getInitParameter("configDirectory");
+			
+			if (!configDirectory.endsWith("/")) {
+				configDirectory += "/";
+			}
+			
+			
+			
+			System.getProperties();
+			
 			RuntimeContext.setEnviroment(enviroment);
 			String path = event.getServletContext().getRealPath("/");
 			RuntimeContext.setConfiguration(AdServerConstants.CONFIG.PATHES, AdServerConstants.PATHES.WEB, path);
-			RuntimeContext.getProperties().load(new FileReader(path + "/WEB-INF/config_" + enviroment +  ".properties"));
+			RuntimeContext.getProperties().load(new FileReader(configDirectory + "config.properties"));
 			
 			if (enviroment.equalsIgnoreCase("development")) {
 				injector = Guice.createInjector(new DevelopmentModule());
